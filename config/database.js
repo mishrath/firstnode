04a60d3e9834b7951260589ee1a4cdd1b903e0db
mishrath <mishrath@gmail.com>
2016-12-21 14:32:10 +1100

    Delete database.js

--- config/database.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    url: 'mongodb://ishazuremongo:5uOzu6PX5LwEkMOfj7AvdEgBOmzidXSrpFIXLY1DnAbjWOHJWdLWVQajHeDXGFgcLQXG0G3uojVyt6An5Hxo5A==@ishazuremongo.documents.azure.com:10250/?ssl=true'
-    //url:'https://ishazuremongo.documents.azure.com:443/ishazuremongo/'
-};
